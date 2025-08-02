4440c66457a6a073022759885d040f602c4b5cad
Yong Tang <yong.tang.github@outlook.com>
2025-08-02 16:49:07 +0000

    FIx

--- src/reflective_learning/inference.py
+++ src/reflective_learning/inference.py
@@ -27,11 +27,15 @@ def sequence(
 
         V = model.vocab_size
         S = model.state_size
+        prefix = torch.tensor(prefix, device=device)
         prefix = prefix.reshape(-1, prefix.shape[-2], prefix.shape[-1])  # [B, C, D]
         B = prefix.size(0)
 
         # Normalized weights
-        weights = torch.nn.functional.normalize(weights, p=2, dim=0)  # [S]
+        weights = torch.tensor(weights, device=device)
+        weights = torch.nn.functional.normalize(
+            weights, p=2, dim=0, device=device
+        )  # [S]
 
         token = torch.empty([B, 0], dtype=torch.long, device=device)  # [B, 0]
         for length in range(maximum):
