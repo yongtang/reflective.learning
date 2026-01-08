fab9b9cf76a21a63ff6aad53d6e02d80e4c52fc0
Yong Tang <yong.tang.github@outlook.com>
2026-01-08 05:16:59 +0000

    Metadata finetune

--- src/reflective_learning/tools/mini.py
+++ src/reflective_learning/tools/mini.py
@@ -24,7 +24,6 @@ from reflective_learning.inference import (
 )
 from reflective_learning.launch import launch
 from reflective_learning.tools import metadata
-from reflective_learning.train import dpo, train
 
 action_space = [
     minigrid.core.actions.Actions.done,
@@ -173,77 +172,6 @@ def f_image(env_size, max_steps, goal, start, facing, image):
     return tuple([filename])
 
 
-def f_callback(
-    model_file,
-    choice,
-    interval,
-    distributed,
-    model,
-    progress,
-    device,
-    rank,
-):
-    if rank != 0:
-        return
-    model = (
-        model.module
-        if (
-            distributed
-            and isinstance(
-                model,
-                (
-                    torch.nn.parallel.DataParallel,
-                    torch.nn.parallel.DistributedDataParallel,
-                    torch.distributed.fsdp.FullyShardedDataParallel,
-                ),
-            )
-        )
-        else model
-    )
-
-    if not hasattr(progress, "_meta_index_"):
-        progress._meta_index_ = 0
-
-    if not (
-        progress.n > progress._meta_index_ + interval or progress.n == progress.total
-    ):
-        return
-
-    # keep copy of max_version = 3
-    max_version = 3
-    for i in reversed(range(1, max_version)):
-        src = f"{model_file}.{i:03d}"
-        dst = f"{model_file}.{i+1:03d}"
-        if os.path.exists(src):
-            shutil.move(src, dst)
-
-    # model.pt => model_1.pt
-    shutil.move(
-        f"{model_file}",
-        f"{model_file}.{1:03d}",
-    )
-
-    save = torch.load(f"{model_file}.{1:03d}", map_location="cpu")
-    save[choice] = model.state_dict()
-
-    # save model
-    with tempfile.NamedTemporaryFile(
-        dir=os.path.dirname(model_file), prefix="model.", suffix=".pt", delete=False
-    ) as f:
-        torch.save(save, f)
-        f.flush()
-        os.fsync(f.fileno())
-        fname = f.name
-    os.replace(fname, f"{model_file}")
-
-    progress._meta_index_ += interval
-
-    if progress.n == progress.total:
-        return
-
-    return
-
-
 def f_datum(image, vocab_fn, state_fn, max_steps, encoder, entry):
     assert len(entry["token"]) <= max_steps, f"{max_steps} vs. {entry['token']}"
 
@@ -290,221 +218,6 @@ def f_data(desc, callback, total):
             return data
 
         return list(filter(lambda e: e is not None, map(fn, range(total))))
-
-
-class LearnDataset(torch.utils.data.Dataset):
-    def __init__(self, dataset, datum_fn):
-        super().__init__()
-        self.dataset = dataset
-        self.datum_fn = datum_fn
-
-    def __enter__(self):
-        self.stack = contextlib.ExitStack()
-        self.file = {
-            file: self.stack.enter_context(open(file, "r"))
-            for file in np.unique(self.dataset[:, 2])
-        }
-        return self
-
-    def __exit__(self, exc_type, exc, tb):
-        return self.stack.__exit__(exc_type, exc, tb)
-
-    def __getitem__(self, index):
-        offset, steps, file = self.dataset[index]
-        self.file[file].seek(offset)
-        line = self.file[file].readline()
-        return self.datum_fn(entry=json.loads(line))
-
-    def __len__(self):
-        return len(self.dataset)
-
-
-class FinetuneDataset(torch.utils.data.Dataset):
-    def __init__(self, dataset, datum_fn):
-        super().__init__()
-        self.dataset = dataset
-        self.datum_fn = datum_fn
-
-    def __enter__(self):
-        self.stack = contextlib.ExitStack()
-        self.file = {
-            file: self.stack.enter_context(open(self.data, "r"))
-            for file in np.unique(self.dataset[:, :, 2])
-        }
-        return self
-
-    def __exit__(self, exc_type, exc, tb):
-        return self.stack.__exit__(exc_type, exc, tb)
-
-    def __getitem__(self, index):
-        (offset_a, steps_a, file_a), (offset_b, steps_b, file_b) = self.dataset[index]
-        self.file[file_a].seek(offset_a)
-        line_a = self.file[file_a].readline()
-        self.file[file_b].seek(offset_b)
-        line_b = self.file[file_b].readline()
-
-        return (
-            self.datum_fn(entry=json.loads(line_a)),
-            self.datum_fn(entry=json.loads(line_b)),
-        )
-
-    def __len__(self):
-        return len(self.dataset)
-
-
-def f_learn(
-    model_file,
-    dataset_file,
-    datum_fn,
-    choice,
-    total,
-    batch,
-    interval,
-    lr,
-    device,
-    rank,
-    world_size,
-    distributed,
-):
-    print(f"Load model: {model_file} ({choice})")
-    info, model = metadata.load(model_file, choice)
-    encoder = ContextEncoder.from_pretrained(info["context"], device=device)
-
-    dataset = np.load(dataset_file, allow_pickle=True)
-    with LearnDataset(
-        dataset=dataset,
-        datum_fn=functools.partial(
-            datum_fn,
-            vocab_fn=lambda e: info["vocab"][e],
-            state_fn=lambda e: metadata.state_space.index(e),
-            max_steps=info["max"],
-            encoder=encoder,
-        ),
-    ) as dataset:
-        collate_fn = model.collate
-
-        sampler = None
-        model.to(device)
-        if distributed:
-            if device.type == "cuda":
-                model = torch.nn.parallel.DistributedDataParallel(
-                    model, device_ids=[device.index], output_device=device.index
-                )
-            else:
-                model = torch.nn.parallel.DistributedDataParallel(model)
-
-            sampler = torch.utils.data.distributed.DistributedSampler(
-                dataset, num_replicas=world_size, rank=rank, shuffle=True
-            )
-            # no epoch loop so no sampler.set_epoch(0)
-
-            # keep per-rank cap consistent with shard size (optional but safe)
-            total = min(total, len(sampler))
-
-        loader = torch.utils.data.DataLoader(
-            dataset,
-            batch_size=batch,
-            collate_fn=collate_fn,  # use captured collate
-            sampler=sampler,
-            shuffle=False,
-            drop_last=False,
-        )
-        optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-        train(
-            model=model,
-            loader=loader,
-            optimizer=optimizer,
-            total=total,
-            callback=functools.partial(
-                f_callback,
-                model_file=model_file,
-                choice=choice,
-                interval=interval,
-                distributed=distributed,
-            ),
-            device=device,
-            rank=rank,
-            desc=f"Learn {choice}",
-        )
-
-
-def f_finetune(
-    model_file,
-    dataset_file,
-    datum_fn,
-    choice,
-    total,
-    batch,
-    interval,
-    lr,
-    device,
-    rank,
-    world_size,
-    distributed,
-):
-    print(f"Load model: {model_file}")
-    info, baseline, finetune = metadata.load(model_file, [choice, choice])
-    encoder = ContextEncoder.from_pretrained(info["context"], device=device)
-
-    dataset = np.load(dataset_file, allow_pickle=True)
-    with FinetuneDataset(
-        dataset=dataset,
-        datum_fn=functools.partial(
-            datum_fn,
-            vocab_fn=lambda e: info["vocab"][e],
-            state_fn=lambda e: metadata.state_space.index(e),
-            max_steps=info["max"],
-            encoder=encoder,
-        ),
-    ) as dataset:
-        collate_fn = finetune.collate
-
-        sampler = None
-        baseline.to(device)
-        finetune.to(device)
-        if distributed:
-            if device.type == "cuda":
-                finetune = torch.nn.parallel.DistributedDataParallel(
-                    finetune, device_ids=[device.index], output_device=device.index
-                )
-            else:
-                finetune = torch.nn.parallel.DistributedDataParallel(finetune)
-
-            sampler = torch.utils.data.distributed.DistributedSampler(
-                dataset, num_replicas=world_size, rank=rank, shuffle=True
-            )
-            # no epoch loop so no sampler.set_epoch(0)
-
-            # optional: cap per-rank total to shard size
-            total = min(total, len(sampler))
-
-        loader = torch.utils.data.DataLoader(
-            dataset,
-            batch_size=batch,
-            collate_fn=lambda batch: tuple(
-                map(collate_fn, zip(*batch))
-            ),  # use captured collate
-            sampler=sampler,
-            shuffle=False,
-            drop_last=False,
-        )
-        optimizer = torch.optim.Adam(finetune.parameters(), lr=lr)
-        dpo(
-            baseline=baseline,
-            finetune=finetune,
-            loader=loader,
-            optimizer=optimizer,
-            total=total,
-            callback=functools.partial(
-                f_callback,
-                model_file=model_file,
-                choice=choice,
-                interval=interval,
-                distributed=distributed,
-            ),
-            device=device,
-            rank=rank,
-        )
 
 
 def fn_prefix(entries, info, image, f_prefix, index):
@@ -758,7 +471,7 @@ def run_learn(choice, data, image, total, batch, interval, lr, device, distribut
 
         if distributed:
             launch(
-                callback=f_learn,
+                callback=metadata.learn,
                 model_file=model_file,
                 dataset_file=dataset_file,
                 datum_fn=functools.partial(f_datum, image),
@@ -774,7 +487,7 @@ def run_learn(choice, data, image, total, batch, interval, lr, device, distribut
             assert len(device) == 1, device
             device = next(iter(device))
 
-            f_learn(
+            metadata.learn(
                 model_file=model_file,
                 dataset_file=dataset_file,
                 datum_fn=functools.partial(f_datum, image),
@@ -984,7 +697,7 @@ def run_finetune(data, image, total, batch, interval, lr, device, distributed):
 
         if distributed:
             launch(
-                callback=f_finetune,
+                callback=metadata.finetune,
                 model_file=model_file,
                 dataset_file=dataset_file,
                 datum_fn=functools.partial(f_datum, image),
@@ -1000,7 +713,7 @@ def run_finetune(data, image, total, batch, interval, lr, device, distributed):
             assert len(device) == 1, device
             device = next(iter(device))
 
-            f_finetune(
+            metadata.finetune(
                 model_file=model_file,
                 dataset_file=dataset_file,
                 datum_fn=functools.partial(f_datum, image),
